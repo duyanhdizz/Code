0993d614d5f1278159255302cc2c207dc4a5c081
duyanhdizz <117369237+duyanhdizz@users.noreply.github.com>
2023-07-13 09:23:19 +0700

    A.cpp

--- 2023_tqvp_02/A.cpp
+++ 2023_tqvp_02/A.cpp
@@ -1,92 +1,44 @@
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
-#pragma GCC optimize("Ofast,O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-#define FOR(i, a, b) for (int i = (a); i <= (int) (b); ++i)
-#define FORD(i, a, b) for (int i = (a); i >= (int) (b); --i)
-#define REP(i, a, b) for (int i = (a); i < (int) (b); ++i)
-#define each(a, x) for (auto &a : x)
-#define fi first
-#define se second
-#define pb push_back
-#define ll long long
-#define all(a) a.begin(),a.end()
-typedef pair<int, int> pii;
-const int N = 2106 + 1e6;
-const int INF = 2106 + 1e9;
-
 const int mod = 1e9 + 7;
+const int N = 6e6 + 5;
+int n, cnt, p[N];
+typedef long long ll;
+bool vis[100000001];
+int main() {
+#define taskname "FMIN"
 
-vector<int> prime;
-vector<int> lpf;
-void sieve(int n) {
-	prime.assign(1, 2);
-	lpf.assign(n + 1, 2);
+    if (fopen(taskname".inp", "r")) {
+        freopen(taskname".inp", "r", stdin);
+        freopen(taskname".out", "w", stdout);
+    }
 
-	for (int x = 3; x <= n; x += 2) {
-		if (lpf[x] == 2) prime.push_back(lpf[x] = x);
-		for (int i = 0; i < prime.size() && prime[i] <= lpf[x] && prime[i] * x <= n; ++i)
-			lpf[prime[i] * x] = prime[i];
-	}
-}
+    cin.tie(0)->sync_with_stdio(0);
 
-long long binpowmod(long long a, long long b, long long m) {
-	a %= m;
-	long long res = 1;
-	while (b > 0) {
-		if (b & 1)
-			res = res * a % m;
-		a = a * a % m;
-		b >>= 1;
-	}
-	return res;
-}
 
-long long binpow(long long a, long long b) {
-	long long res = 1;
-	while (b > 0) {
-		if (b & 1)
-			res = res * a;
-		a = a * a;
-		b >>= 1;
-	}
-	return res;
-}
+    cin >> n;
+    long long ans = 1;
 
-ll solve(ll k, ll m) {
-	int _ = 1;
-	while(binpow(k, _) <= m) _++;
-	return _ - 1;
-}
+    for (int i = 2; i <= n; i++) {
+        if (!vis[i]) { // nếu i là số nguyên tố
+            p[cnt++] = i; // thêm i vào danh sách các số nguyên tố
 
-void SaKaTa() {
-	int n;
-	cin >> n;
-	sieve(n);
-	ll ans = 1;
-	each(p, prime) {
-		ans = ans * binpowmod(p, solve(p, n), mod) % mod;
-	}
-	cout << ans;
-}
+            for (ll s = i; s <= n; s *= i) // tính luỹ thừa t lớn nhất sao cho i^t <= n, sẽ nhân i^t vào kết quả
+                ans = ans * i % mod;
+        }
 
-int32_t main() {
-#define TASKNAME "FMIN"
-	ios_base::sync_with_stdio (false);
-	cin.tie (nullptr);
-	cout.tie (nullptr);
-	int tbegin = clock();
-	if ( fopen( TASKNAME".inp", "r" ) ) {
-		freopen (TASKNAME".inp", "r", stdin);
-		freopen (TASKNAME".out", "w", stdout);
-	}
-	int testcase = 1;
-//    cin >> testcase;
-	while(testcase--)
-		SaKaTa();
-	cerr << '\n' << "Runtime: " << (clock() - tbegin) << " ms\n";
-	return 0;
+        // sàng o(n), đây là code rút gọn từ link trên
+        for (int j = 0; j < cnt; j++) { // duyệt qua danh sách các số nguyên tố
+            ll v = i * p[j];
+            if (v > n) 
+                break;
+            vis[v] = 1;
+
+            if (i % p[j] == 0) // kiểm tra xem số p[j] có phải số nguyên tố nhỏ nhất mà i chia hết không thì dừng lại
+            // như vậy đảm bảo mỗi số chỉ được thăm đúng 1 lần
+                break;
+        }
+    }
+
+    cout << ans << endl;
 }
